a802494335b56d41a2773997c23d30b052b2ac27
mstarkov <starsky.mik@gmail.com>
2022-05-18 20:58:52 +0200

    refactor

--- test/lesson/pointers-and-errors/wallet_withdraw_test.go
+++ test/lesson/pointers-and-errors/wallet_withdraw_test.go
@@ -12,3 +12,12 @@ func TestWallet_Withdraw(t *testing.T) {
 	testingTools.AssertNoError(t, wallet.Withdraw(pointersAndErrors.Bitcoin(10)))
 	testingTools.AssertEquals(t, wallet.Balance(), pointersAndErrors.Bitcoin(10))
 }
+
+func TestWallet_Withdraw_Errors(t *testing.T) {
+	wallet := pointersAndErrors.Wallet{}
+	wallet.Deposit(pointersAndErrors.Bitcoin(10))
+	err := wallet.Withdraw(pointersAndErrors.Bitcoin(100))
+
+	testingTools.AssertEquals(t, wallet.Balance(), pointersAndErrors.Bitcoin(10))
+	testingTools.AssertError(t, err, pointersAndErrors.InsufficientBalanceError)
+}
