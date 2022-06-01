50a74bde7903dda4303380efaa5139cdcb3eb29a
mstarkov <starsky.mik@gmail.com>
2022-06-01 20:36:12 +0200

    fix error

--- internal/pkg/testing_tools/assert_equals.go
+++ internal/pkg/testing_tools/assert_equals.go
@@ -6,6 +6,6 @@ func AssertEquals(t testing.TB, actual, expected any) {
 	t.Helper()
 
 	if actual != expected {
-		t.Errorf("expected '%s' but got '%s'", expected, actual)
+		t.Errorf("expected '%v' but got '%v'", expected, actual)
 	}
 }
